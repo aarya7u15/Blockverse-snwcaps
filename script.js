f8221ae129d80405270526bcf0b2f77829da0b5e
Aarya Upadhyay <aarya7u@gmail.com>
2025-12-06 14:45:33 +0530

    not found message added in search bar

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const searchInput = document.getElementById("searchInput");
 const gameCards = document.querySelectorAll(".game-card");
+const noResult = document.getElementById("noResult");
 
 searchInput.addEventListener("keyup", function() {
   const value = searchInput.value.toLowerCase();
-
+  let found = false;
   gameCards.forEach(card => {
     const gameName = card.querySelector("h3").textContent.toLowerCase();
 
     if (gameName.includes(value)) {
       card.style.display = "block";
+      found = true;
     } else {
       card.style.display = "none";
     }
   });
+  if (!found) {
+    noResult.style.display = "block";
+  } else {
+    noResult.style.display = "none";
+  }
 });
+
+
